1fa20d4432c165c61c327c4a7b32d24bd077cab5
Tyler Strang <tylerstrang@hotmail.com>
2021-11-05 19:54:48 -0230

    added some of the logic for the bricks

--- breakout/breakout.js
+++ breakout/breakout.js
@@ -1,115 +1,157 @@
-//add all rectangles with x y coordinates saved so we can check collision
-
-
+//event listeners for moving left and right
 document.addEventListener('keydown', (event) => {
-    if(event.key == "ArrowLeft" || event.key == "a"){
+    if (event.key == "ArrowLeft" || event.key == "a") {
         playerLeft = true
     }
-    else if(event.key == "ArrowRight" || event.key == "d"){
+    else if (event.key == "ArrowRight" || event.key == "d") {
         playerRight = true
     }
-  }, false);
-  document.addEventListener('keyup', (event) => {
-    if(event.key == "ArrowLeft" || event.key == "a"){
+}, false);
+document.addEventListener('keyup', (event) => {
+    if (event.key == "ArrowLeft" || event.key == "a") {
         playerLeft = false
     }
-    else if(event.key == "ArrowRight" || event.key == "d"){
+    else if (event.key == "ArrowRight" || event.key == "d") {
         playerRight = false
     }
-    
-  }, false);
+}, false);
 
-var canvas = document.getElementById("theCanvas");
-var ctx = canvas.getContext("2d");
-var minSpeedSlider = document.getElementById("minSpeedSlider");
-var minSpeedValue = document.getElementById("minSpeedValue");
+//grabbing page elements
+let canvas = document.getElementById("theCanvas");
+let ctx = canvas.getContext("2d");
+let minSpeedSlider = document.getElementById("minSpeedSlider");
+let minSpeedValue = document.getElementById("minSpeedValue");
+let maxSpeedSlider = document.getElementById("maxSpeedSlider");
+let maxSpeedValue = document.getElementById("maxSpeedValue");
 
-var x = canvas.width / 2;
-var y = canvas.height - 30;
-var dxy = [-3, -3];
-var minSpeed = 2;
-var maxSpeed = 5;
-var count = 0; 
-var radius = 10;
-var playerWidth = 40;
-var playerHeight = 10;
-var playerX = x;
-var playerY = y;
-var playerSpeed = 3;
-var playerLeft = false;
-var playerRight = false;
 
-minSpeedSlider.oninput = function() {
+//ball values
+let xBall = canvas.width / 2;
+let yBall = canvas.height - 30;
+let dxy = [-3, -3];
+let minSpeed = 2;
+let maxSpeed = 5;
+let radius = 10;
+
+//player variables
+let playerWidth = 40;
+let playerHeight = 10;
+let playerX = xBall;
+let playerY = yBall;
+let playerSpeed = 3;
+let playerLeft = false;
+let playerRight = false;
+
+//brick variables
+let brickHp = 3;
+let brickWidth = 50;
+let brickHeight = 20;
+let brickRows = 4;
+let brickArray = [];
+
+//creating a brick object constructor
+function BreakoutBrick(x,y) {
+    this.xCoordinate = x;
+    this.yCoordinate = y;
+    this.hp = brickHp;
+    this.width = brickWidth;
+    this.height = brickHeight;
+}
+
+
+//update min speed based on user input to slider
+minSpeedSlider.oninput = function () {
     minSpeed = this.value;
     minSpeedValue.innerHTML = this.value;
 }
 
-//Check collision with game window, and update velocity of ball if collision occurs
-function checkCollisonBallWindow() {
-    var randNum = Math.random();
-    var xCollisonLeft = x - radius <= 0;
-    var xCollisonRight =  x + radius >= canvas.width;
-    var yCollisonUp =  y - radius <= 0;
-    var yCollisionDown = y + radius >= canvas.height;
+//update max speed based on user input to slider
+maxSpeedSlider.oninput = function () {
+    maxSpeed = this.value;
+    maxSpeedValue.innerHTML = this.value;
+}
 
-    if((xCollisonLeft || xCollisonRight) && (yCollisonUp || yCollisionDown)){
-        x -= dxy[0];
-        y -= dxy[1];
+//populating array of brick objects
+createBrickArray = () => {
+    xIndex = 0;
+    rowIndex = 0;
+    while(rowIndex <= brickRows) {
+        while(xIndex + brickWidth <= canvas.width) {
+            newBrick = BreakoutBrick(xIndex * brickWidth, rowIndex * brickHeight);
+            brickArray.append(newBrick);
+        }
+    }
+}
+
+//Check collision with game window, and update velocity of ball if collision occurs
+checkCollisonBallWindow = () => {
+    let randNum = Math.random();
+    let xCollisonLeft = xBall - radius <= 0;
+    let xCollisonRight = xBall + radius >= canvas.width;
+    let yCollisonUp = yBall - radius <= 0;
+    let yCollisionDown = yBall + radius >= canvas.height;
+
+    if ((xCollisonLeft || xCollisonRight) && (yCollisonUp || yCollisionDown)) {
+        xBall -= dxy[0];
+        yBall -= dxy[1];
         dxy[0] *= -(randNum + 0.5);
         dxy[1] *= -(randNum + 0.5);
-        x += Math.sin
+        xBall += Math.sin
     }
     if (xCollisonLeft || xCollisonRight) {
-        console.log(x);
+        console.log(xBall);
         console.log(dxy[0]);
-        console.log(y);
+        console.log(yBall);
         console.log(dxy[1]);
-        x -= dxy[0];
+        xBall -= dxy[0];
         dxy[0] *= -(randNum + 0.5)
         dxy[1] *= (randNum + 0.5)
     }
     if (yCollisonUp || yCollisionDown) {
-        y -= dxy[1];
+        yBall -= dxy[1];
         dxy[0] *= (randNum + 0.5)
         dxy[1] *= -(randNum + 0.5)
     }
 
-    if(Math.abs(dxy[0]) < minSpeed) {
-        dxy[0] = Math.sign(dxy[0]) * minSpeed; 
+    if (Math.abs(dxy[0]) < minSpeed) {
+        dxy[0] = Math.sign(dxy[0]) * minSpeed;
     }
-    if(Math.abs(dxy[1]) < minSpeed) {
-        dxy[1] = Math.sign(dxy[1]) * minSpeed; 
+    if (Math.abs(dxy[1]) < minSpeed) {
+        dxy[1] = Math.sign(dxy[1]) * minSpeed;
     }
-    if(Math.abs(dxy[0]) > maxSpeed) {
-        dxy[0] = Math.sign(dxy[0]) * maxSpeed; 
+    if (Math.abs(dxy[0]) > maxSpeed) {
+        dxy[0] = Math.sign(dxy[0]) * maxSpeed;
     }
-    if(Math.abs(dxy[1]) > maxSpeed) {
-        dxy[1] = Math.sign(dxy[1]) * maxSpeed; 
+    if (Math.abs(dxy[1]) > maxSpeed) {
+        dxy[1] = Math.sign(dxy[1]) * maxSpeed;
     }
 }
 
-function checkCollisionPlayerWindow() {
-    if(playerX < 0){
+checkCollisionPlayerWindow = () => {
+    if (playerX < 0) {
         playerX = 0;
     }
-    else if(playerX + playerWidth > canvas.width){
+    else if (playerX + playerWidth > canvas.width) {
         playerX = canvas.width - playerWidth;
     }
 }
-function updatePositions(){
-    x += dxy[0];
-    y += dxy[1];
-    if(playerLeft){
+updatePositions = () => {
+    xBall += dxy[0];
+    yBall += dxy[1];
+    if (playerLeft) {
         playerX -= playerSpeed;
+    }
+    if (playerRight) {
+        playerX += playerSpeed;
     }
 }
 
-function gameUpdate() {
+gameUpdate = () => {
     //move into a function that draws all objects on screen
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI*2);
-    ctx.rect(playerX, playerY, playerWidth,playerHeight);
+    ctx.arc(xBall, yBall, radius, 0, Math.PI * 2);
+    ctx.rect(playerX, playerY, playerWidth, playerHeight);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -119,11 +161,11 @@ function gameUpdate() {
     //check ball - rectangle collison
     //update
     updatePositions()
-    
+
 
 }
 
-function main() {
+main = () => {
     setInterval(gameUpdate, 10);
 }
 main();
